1f971ac485c80c731b8b3ee937e987c166a68b8e
Bruno <c.brunomelo@outlook.com>
2024-01-27 19:38:42 -0300

    refatorado classe repositoryBase

--- TruckDriver.Infra.DAO/RepositoryBase.cs
+++ TruckDriver.Infra.DAO/RepositoryBase.cs
@@ -23,35 +23,14 @@ namespace TruckDriver.Infra.ADO
                 
                 var map = entity.GetMap().Insert();
 
-                string campos = "";
-                string values = "";
-                int i = 1;
-
-                foreach (var prop in entity.GetType().GetProperties())
-                {
-                    if (!map.ContainsKey(prop.Name))
-                        continue;
-                    campos += map[prop.Name] + ", ";
-                    values += $"@valor{i}, ";
-                    i++;
-                }
-
-                campos = campos.Substring(0, campos.Length - 2);
-                values = values.Substring(0, values.Length - 2);
-
-
+                string campos = MontarCampos(entity, map);
+                string values = MontarValues(entity, map);
+                
 
                 string sql = $"INSERT INTO [{map["TABLE"]}] ({campos}) VALUES ({values}); SELECT last_insert_rowid();";
                 SqliteCommand command = new SqliteCommand(sql, connection);
 
-                i = 1;
-                foreach (var prop in entity.GetType().GetProperties())
-                {
-                    if(!map.ContainsKey(prop.Name))
-                        continue;
-                    command.Parameters.AddWithValue($"valor{i}", prop.GetValue(entity) ?? DBNull.Value);
-                    i++;
-                }
+                AdicionarParametros(entity, map, command);
 
 
                 long idInserido = (long)command.ExecuteScalar();
@@ -63,5 +42,45 @@ namespace TruckDriver.Infra.ADO
             }
         }
 
+        private void AdicionarParametros(TEntity entity, IDictionary<string, string> map, SqliteCommand command)
+        {
+            int i = 1;
+            foreach (var prop in entity.GetType().GetProperties())
+            {
+                if (map.ContainsKey(prop.Name))
+                {
+                    command.Parameters.AddWithValue($"@valor{i}", prop.GetValue(entity) ?? DBNull.Value);
+                    i++;
+                }
+            }
+        }
+
+        private string MontarValues(TEntity entity, IDictionary<string, string> map)
+        {
+            StringBuilder valoresBuilder = new StringBuilder();
+            int i = 1;
+            foreach (var prop in entity.GetType().GetProperties())
+            {
+                if (map.ContainsKey(prop.Name))
+                {
+                    valoresBuilder.Append($"@valor{i}, ");
+                    i++;
+                }
+            }
+            return valoresBuilder.ToString().TrimEnd(' ', ',');
+        }
+
+        private string MontarCampos(TEntity entity, IDictionary<string, string> map)
+        {
+            StringBuilder campos = new StringBuilder();
+
+            foreach (var prop in entity.GetType().GetProperties())
+            {
+                if (map.ContainsKey(prop.Name))
+                    campos.Append(map[prop.Name] + ", ");
+            }
+            return campos.ToString().TrimEnd(',', ' ');
+
+        }
     }
 }
